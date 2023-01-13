4333ec5f5c68bc9eefbf0378f0e7b5e0a1938dc4
mihir28pf <112117331+mihir28pf@users.noreply.github.com>
2023-01-14 00:32:11 +0530

    Update client.go

--- database/client.go
+++ database/client.go
@@ -3,7 +3,7 @@ package database
 import (
 	"golang-crud-rest-api/entities"
 	"log"
-
+         "fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -14,6 +14,7 @@ var err error
 func Connect(connectionString string) {
 	Instance, err = gorm.Open(mysql.Open("root:root@tcp(127.0.0.1:3306)/crud_demo"), &gorm.Config{})
 	if err != nil {
+		fmt.Println("\"line no 17 - client go\"")
 		log.Fatal(err)
 		panic("Cannot connect to DB")
 	}
